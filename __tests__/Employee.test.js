afdc3b0be7f0b29eb4d7975d3cb7fd09b315cf8c
Nick <nick.alex.ritchie@gmail.com>
2021-08-07 10:53:05 +1000

    rebuilt employee parent class to take keyword args

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,78 +4,81 @@ const Exceptions = require('../lib/Exceptions');
 // top level test of Employee class
 describe('Employee', () => {
     // testing initialization
+    var validEmployee = {name: 'James', id:'1', email:'validEmail'};
     describe('Initialization', () => {
         // Happy Path
-        it("should return a name string", () => {
-            const employee = new Employee('James', '1', 'validEmail');
+        it("HP should return a name string", () => {
+            const employee = new Employee(validEmployee);
             expect(employee.name).toBe('James');
         });
-        it("should return a id integer", () => {
-            const employee = new Employee('James', '1', 'validEmail');
+        it("HP should return a id integer", () => {
+            const employee = new Employee(validEmployee);
             expect(employee.id).toBe(1);
         });
-        it("should return an email string", () => {
-            const employee = new Employee('James', '1', 'validEmail');
+        it("HP should return an email string", () => {
+            const employee = new Employee(validEmployee);
             expect(employee.email).toBe('validEmail');
         });
 
         // Missing arguments
         it("UHP should raise a MissingArgumentError if no arguments", () => {
             expect(()=> {
-                new Employee();
+                new Employee({});
             }).toThrowError(Exceptions.MissingArgumentError);
         });
         it("UHP should raise a MissingArgumentError if missing id", () => {
             expect(() => {
-                new Employee('Jarrod', 'email');
+                new Employee({name: 'Jarrod', email:'email'});
             }).toThrowError(Exceptions.MissingArgumentError);
         });
         it("UHP should raise a MissingArgumentError if missing email", () => {
             expect(()=> {
-                new Employee('Jarrod', 4);
+                new Employee({name: 'Jarrod', id:4});
             }).toThrowError(Exceptions.MissingArgumentError);
         });
+
         // incorrect types for arguments - str, int, str
         it("UHP should raise a BadArgumentError if bad name", () => {
             expect(()=> {
-                new Employee(0, 'BadId-ShouldBeInt', 'email');
+                new Employee({name:0, id:1, email:'email'});
             }).toThrowError(Exceptions.BadArgumentError);
         });
         it("UHP should raise a BadArgumentError if bad id", () => {
             expect(()=> {
-                new Employee('Name', 'BadId-ShouldBeInt', 'email');
+                new Employee({name:'ValidName', id:'bad int', email:'email'});
             }).toThrowError(Exceptions.BadArgumentError);
         });
         it("UHP should raise a BadArgumentError if bad email", () => {
             expect(()=> {
-                new Employee('Name', 'BadId-ShouldBeInt', {badEmail:'ThisShouldBeAStringNotAnObject'});
+                new Employee({name: 'Name', id: 'BadId-ShouldBeInt', email: {badEmail:'ThisShouldBeAStringNotAnObject'}});
             }).toThrowError(Exceptions.BadArgumentError);
         });
     });
     //testing get name method
+
     describe('get name method', () => {
-        const employee = new Employee('Jemima', 2, 'anotherValidEmail');
+        const employee = new Employee(validEmployee);
         it("should return a name string", () => {
-            expect(employee.getName()).toBe('Jemima');
+            expect(employee.getName()).toBe('James');
         });
     });
     // testing get id method
     describe('get id method', () => {
-        const employee = new Employee('Jemima', 2, 'anotherValidEmail');
+        const employee = new Employee(validEmployee);
         it("should return an id integer", () => {
-            expect(employee.getId()).toBe(2);
+            expect(employee.getId()).toBe(1);
         });
     });
     // testing get email method
     describe('get email method', () => {
-        const employee = new Employee('Jemima', 2, 'anotherValidEmail');
+        const employee = new Employee(validEmployee);
         it("should return an email string", () => {
-            expect(employee.getEmail()).toBe('anotherValidEmail');
+            expect(employee.getEmail()).toBe('validEmail');
         });
     });
     // testing get role method
     describe('get role method', () => {
-        const employee = new Employee('Jemima', 2, 'anotherValidEmail');
+        const employee = new Employee(validEmployee);
         it("should return a role string", () => {
             expect(employee.getRole()).toBe('Employee');
         });
